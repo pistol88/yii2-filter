a74dec21f8804546226d032167b818efacbe4af3
pistol88 <pistol88@gmail.com>
2016-05-14 13:39:20 +0500

    fix

--- views/filter/create.php
+++ views/filter/create.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Settings */
 
-$this->title = 'Создание настройки';
+$this->title = 'Создание фильтра';
 $this->params['breadcrumbs'][] = ['label' => 'Фильтры', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
