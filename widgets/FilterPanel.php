9d538f2dacc648c7330e0e16a5a1630b2e07d4d0
pistol88 <pistol88@gmail.com>
2016-06-13 17:17:50 +0500

    return widget

---
+++ widgets/FilterPanel.php
@@ -0,0 +1,72 @@
+<?php
+namespace pistol88\filter\widgets;
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use pistol88\filter\models\Filter;
+use pistol88\filter\models\FieldRelationValue;
+use yii;
+
+class FilterPanel extends \yii\base\Widget
+{
+    public $itemId = NULL;
+    public $filterId = NULL;
+    public $itemCssClass = 'item';
+    public $fieldName = 'filter';
+    public $blockCssClass = 'block';
+    public $submitButtonValue = 'Показать';
+
+    public function init()
+    {
+        parent::init();
+
+        \pistol88\filter\assets\FrontendAsset::register($this->getView());
+    }
+
+    public function run()
+    {
+        $params = ['is_filter' => 'yes'];
+        
+        if($this->filterId) {
+            $params['id'] = $this->filterId;
+        }
+
+        $filters = Filter::find()->andWhere($params)->all();
+        
+        $return = [];
+        foreach($filters as $filter) {
+            if(in_array($this->itemId, $filter->selected)) {
+                $block = '';
+                $title = Html::tag('p', $filter->name, ['class' => 'heading']);
+                foreach($filter->variants as $variant) {
+                    $checked = false;
+                    if($filterData = yii::$app->request->get('filter')) {
+                        if(isset($filterData[$filter->id]) && (isset($filterData[$filter->id][$variant->id]) |  $filterData[$filter->id] == $variant->id)) {
+                            $checked = true;
+                        }
+                    }
+
+					if(!in_array($filter->type, array('radio', 'checkbox'))) {
+						$filter->type = 'checkbox';
+					}
+                    
+                    if($filter->type == 'radio') {
+                        $fieldName = $this->fieldName.'['.$filter->id.']';
+                    } else {
+                        $fieldName = $this->fieldName.'['.$filter->id.']['.$variant->id.']';
+                    }
+
+                    $field = Html::input($filter->type, $fieldName, $variant->id, ['checked' => $checked, 'data-item-css-class' => $this->itemCssClass, 'id' => "variant{$variant->id}"]);
+                    $field .= Html::label($variant->value, "variant{$variant->id}");
+                    $block .= Html::tag('div', $field);
+                }
+                $return[] = Html::tag('div', $title.$block, ['class' => $this->blockCssClass]);
+            }
+            
+        }
+
+        if($return) $return[] = Html::input('submit', 'submit', $this->submitButtonValue, ['class' => 'btn btn-submit']);
+        
+        return Html::tag('form', implode('', $return), ['name' => 'pistol88-filter', 'action' => '', 'class' => 'pistol88-filter']);
+    }
+}
